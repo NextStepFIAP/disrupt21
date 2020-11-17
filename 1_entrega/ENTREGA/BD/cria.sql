7649c49eaebd0d731b70f62c76e4ad19c88359ca
GuiRodriguero <gui.rodriguero@hotmail.com>
2020-11-17 00:00:44 -0300

    Adicionando colunas em ingles; modelo fisico atualizado; png modelo fisico

--- 1_entrega/ENTREGA/BD/cria.sql
+++ 1_entrega/ENTREGA/BD/cria.sql
@@ -1,9 +1,11 @@
 CREATE TABLE t_nxt_evento (
-    id_evento  NUMBER(2) NOT NULL,
-    nm_evento  VARCHAR2(128) NOT NULL,
-    dt_evento  DATE NOT NULL,
-    url_foto   VARCHAR2(512) NOT NULL,
-    ds_evento  VARCHAR2(600) NOT NULL
+    id_evento    NUMBER(2) NOT NULL,
+    nm_evento    VARCHAR2(128) NOT NULL,
+    nm_en_evento VARCHAR2(128) NOT NULL,
+    dt_evento    DATE NOT NULL,
+    url_foto     VARCHAR2(512) NOT NULL,
+    ds_evento    VARCHAR2(600) NOT NULL,
+    ds_en_evento VARCHAR2(600) NOT NULL
 );
 
 ALTER TABLE t_nxt_evento ADD CONSTRAINT t_nxt_evento_pk PRIMARY KEY ( id_evento );
@@ -13,13 +15,14 @@ START WITH 1
 INCREMENT BY 1;
 
 CREATE TABLE t_nxt_personagem (
-    id_personagem     NUMBER(2) NOT NULL,
-    nm_personagem     VARCHAR2(64) NOT NULL,
-    nm_ator           VARCHAR2(64) NOT NULL,
-    url_foto          VARCHAR2(512) NOT NULL,
-    dt_nascimento     DATE NOT NULL,
-    ds_apresentacao   VARCHAR2(600) NOT NULL,
-    nm_classificacao  VARCHAR2(32) NOT NULL
+    id_personagem        NUMBER(2) NOT NULL,
+    nm_personagem        VARCHAR2(64) NOT NULL,
+    nm_ator              VARCHAR2(64) NOT NULL,
+    url_foto             VARCHAR2(512) NOT NULL,
+    dt_nascimento        DATE NOT NULL,
+    ds_apresentacao      VARCHAR2(600) NOT NULL,
+    ds_en_apresentacao   VARCHAR2(600) NOT NULL,
+    nm_classificacao     VARCHAR2(32) NOT NULL
 );
 
 ALTER TABLE t_nxt_personagem ADD CONSTRAINT t_nxt_personagem_pk PRIMARY KEY ( id_personagem );
